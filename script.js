49e1ca0a262d8e6c483c79676ead0962c2de0c9f
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-04 15:37:57 -0300

    Criando uma classe com um método construtor

--- script.js
+++ script.js
@@ -9,9 +9,20 @@
  * e herança de protótipos (syntax sugar).
  */
 
-
 /*
 Nesta aula, vamos aprender o conceito de classes em JavaScript, introduzido no ECMAScript 2015. Classes são uma forma de criar objetos e definir comportamentos usando construtores e métodos. Elas oferecem uma sintaxe mais amigável para criar objetos e herdar protótipos, conhecida como "syntax sugar". 
 As classes têm construtores e métodos, permitindo a criação de modelos para objetos. Além disso, as classes possibilitam a herança de propriedades e métodos de outras classes, promovendo a reutilização de código.
 */
 
+/*
+Nesta aula, aprenderemos a criar uma classe em JavaScript, utilizando a palavra reservada class seguida do nome da classe no padrão Paschal Case. Vamos entender um pouco a diferença entre Camel Case, Snake Case e Paschal Case. Iremos definir o corpo da classe e criar um construtor, que é uma função especial executada automaticamente ao instanciar a classe.
+*/
+
+// Criando uma classe com um método construtor
+class Person {
+  constructor(name) {
+    console.log("Olá", name);
+  }
+}
+
+const person = new Person("Jonas");
