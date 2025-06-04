46d1a28207389ca7aed5c61cc3c67f8ecaf9404c
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-04 15:33:12 -0300

    Aprendendo sobre classes

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * Classes
+ *
+ * No JavaScript, as classes são uma forma de criar objetos
+ * e definir seu comportamento por meio de construtores e métodos.
+ *
+ * Elas foram introduzidas no ECMAScript 2015 (também conhecido como ES6)
+ * para fornecer uma sintaxe mais amigável para a criação de objetos
+ * e herança de protótipos (syntax sugar).
+ */
+
+
+/*
+Nesta aula, vamos aprender o conceito de classes em JavaScript, introduzido no ECMAScript 2015. Classes são uma forma de criar objetos e definir comportamentos usando construtores e métodos. Elas oferecem uma sintaxe mais amigável para criar objetos e herdar protótipos, conhecida como "syntax sugar". 
+As classes têm construtores e métodos, permitindo a criação de modelos para objetos. Além disso, as classes possibilitam a herança de propriedades e métodos de outras classes, promovendo a reutilização de código.
+*/
+
