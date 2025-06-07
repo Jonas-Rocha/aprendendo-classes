6b11218f21df3e4b51382bec29077ef516d72986
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-07 10:19:10 -0300

    Aprendendo sobre herança e cadeia de protótipos

--- script.js
+++ script.js
@@ -111,3 +111,44 @@ cat.makeNoise();
 const dog = new Dog("Yugi");
 console.log(dog.name);
 dog.makeNoise();
+
+/**
+ * Classes x Prototype
+ * JavaScript é dinâmico e não dispõe de uma implementação de uma class
+ * (a palavra-chave class foi introduzida no ES2015, mas é syntax sugar).
+ * O JavaScript permanece baseado em prototype.
+ */
+
+/**
+ * Prototype
+ * Quando se trata de herança, o JavaScript tem somente um construtor: objetos.
+ * Cada objeto tem um link interno para um outro objeto chamado prototype.
+ *
+ * O objeto prototype também tem um atributo prototype, e assim por diante,
+ * até o que o valor null seja encontrado como sendo o seu prototype.
+ *
+ * O null que, por definição, não tem prototype, age como um link final
+ * nesta cadeia de protótipos (prototype chain).
+ */
+
+/**
+ * Resumo
+ *
+ * - Quando um objeto é criado ele possui automaticamente uma propriedade
+ *   que referencia outro objeto e esse objeto é chamado de Prototype.
+ *
+ * - O objeto prototype herda propriedades e métodos do seu protótipo ascendente.
+ *   Essas propriedades não pertencem ao objeto em si, mas sim ao prototype do objeto.
+ *
+ * - O Prototype é o mecanismo pelo qual os objetos JavaScript herdam recursos uns dos outros.
+ *
+ * - A cadeia termina quando chegamos a um protótipo que tem null por protótipo.
+ *
+ * - Um objeto pode utilizar qualquer propriedade ou método que existir nesse encadeamento de Protótipos.
+ */
+
+/*
+Nesta aula, vamos aprender o conceito de prototype chain no JavaScript, que se baseia em herança e cadeia de protótipos. Vamos entender como objetos herdam propriedades e métodos de seus protótipos ascendentes, até chegar a um protótipo com valor nulo, finalizando a cadeia.
+*/
+
+// Aprendendo sobre herança e cadeia de protótipos.
